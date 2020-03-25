3343cd29e6930065eab489b1a70736f424ea0d00
wenren <r0I2O520>
2020-03-25 15:41:43 +0800

    cloud-eureka-server7001搭建完成，cloud-provider-payment8001和cloud-consumer-order80融合完成。

--- cloud-consumer-order80/src/main/java/com/wenren/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/wenren/springcloud/OrderMain80.java
@@ -2,6 +2,7 @@ package com.wenren.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author wangyang
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create 2020-03-24-11:16
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud-eureka-server7001/src/main/java/com/wenren/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/wenren/springcloud/EurekaMain7001.java
@@ -2,17 +2,17 @@ package com.wenren.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author wangyang
  * @version V1.0
- * @create 2020-03-20-14:41
+ * @create 2020-03-25-14:14
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
