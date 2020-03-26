33cfba2a73b464d0e99014ad4ac4d7800dd5ebd2
wenren <r0I2O520>
2020-03-26 14:43:52 +0800

    cloud-eureka-server7002搭建完成，Eureka集群搭建完成，cloud-provider-payment8001和cloud-consumer-order80融合完成。

---
+++ cloud-eureka-server7002/src/main/java/com/wenren/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.wenren.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author wangyang
+ * @version V1.0
+ * @create 2020-03-26-12:04
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
