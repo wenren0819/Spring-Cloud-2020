14cccd47e6186b31f6f9209e6adcdcb8aacdd897
wenren <r0I2O520>
2020-03-31 14:59:20 +0800

    cloud-consumerzk-order80搭建完成

---
+++ cloud-consumerzk-order80/src/main/java/com/wenren/springcloud/OrderZKMain80.java
@@ -0,0 +1,18 @@
+package com.wenren.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wangyang
+ * @version V1.0
+ * @create 2020-03-30-15:38
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZKMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class, args);
+    }
+}
---
+++ cloud-consumerzk-order80/src/main/java/com/wenren/springcloud/controller/OrderZKController.java
@@ -0,0 +1,28 @@
+package com.wenren.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @author wangyang
+ * @version V1.0
+ * @create 2020-03-30-15:42
+ */
+@RestController
+@Slf4j
+public class OrderZKController {
+    public static final String INVOKE_URL = "http://cloud-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping(value = "/consumer/payment/zk")
+    public String paymentInfo() {
+        String result = restTemplate.getForObject(INVOKE_URL + "/payment/zk", String.class);
+        return result;
+    }
+}
