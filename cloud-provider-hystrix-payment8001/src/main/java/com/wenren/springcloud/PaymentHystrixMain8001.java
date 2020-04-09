60fcb6ee652779a3194e0e364ac46942c98d079c
wenren <r0I2O520>
2020-04-09 15:42:00 +0800

    cloud-provider-hystrix-payment8001搭建完成。

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/wenren/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,18 @@
+package com.wenren.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wangyang
+ * @version V1.0
+ * @create 2020-04-09-11:56
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class, args);
+    }
+}
---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/wenren/springcloud/service/PaymentService.java
@@ -0,0 +1,33 @@
+package com.wenren.springcloud.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author wangyang
+ * @version V1.0
+ * @create 2020-04-09-11:58
+ */
+@Service
+public class PaymentService {
+    /**
+     * 正常访问，肯定OK
+     *
+     * @param id
+     * @return
+     */
+    public String paymentInfo_OK(Integer id) {
+        return "线程池:" + Thread.currentThread().getName() + " paymentInfo_OK,id:" + id + "\t" + "O(∩_∩)O哈哈~";
+    }
+
+    public String paymentInfo_TimeOut(Integer id) {
+        int timeNumber = 3;
+        try {
+            TimeUnit.SECONDS.sleep(timeNumber);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "线程池:" + Thread.currentThread().getName() + " paymentInfo_TimeOut,id:" + id + "\t" + "O(∩_∩)O哈哈~" + " 耗时（秒）" + timeNumber;
+    }
+}
