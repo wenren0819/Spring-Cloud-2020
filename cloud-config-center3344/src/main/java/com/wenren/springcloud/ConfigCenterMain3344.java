ef5683bfe2ac418ab82a8c19f11ce04147e63cc8
wenren <r0I2O520>
2020-04-30 15:16:33 +0800

    Spring Cloud Config篇完成。

---
+++ cloud-config-center3344/src/main/java/com/wenren/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.wenren.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author wangyang
+ * @version V1.0
+ * @create 2020-04-16-16:45
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
--- cloud-consumer-order80/src/test/java/T.java
+++ cloud-consumer-order80/src/test/java/T.java
@@ -1,3 +1,6 @@
+import java.math.BigDecimal;
+import java.util.UUID;
+
 /**
  * @author wangyang
  * @version V1.0
@@ -6,5 +9,10 @@
 public class T {
     public static void main(String[] args) {
         System.out.println(Integer.MAX_VALUE);
+        BigDecimal amount=new BigDecimal(1051);
+        BigDecimal rate=new BigDecimal(0.9817);
+        BigDecimal result = amount.multiply(rate).divide(new BigDecimal(100)).setScale(0, BigDecimal.ROUND_DOWN);
+        System.out.println(result.multiply(new BigDecimal(100)));
+        System.out.println(UUID.randomUUID());
     }
 }
