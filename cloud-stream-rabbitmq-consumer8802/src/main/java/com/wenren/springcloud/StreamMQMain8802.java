bb9a129a978fdd81189ec03795512fa2c5895907
wenren <r0I2O520>
2020-05-09 15:05:59 +0800

    cloud-stream-rabbitmq-consumer8802搭建完成。

--- cloud-stream-rabbitmq-consumer8802/src/main/java/com/wenren/springcloud/StreamMQMain8802.java
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/wenren/springcloud/StreamMQMain8802.java
@@ -7,12 +7,11 @@ import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 /**
  * @author wangyang
  * @version V1.0
- * @create 2020-05-07-16:09
+ * @create 2020-05-08-14:14
  */
-//多environment下关闭，不然会在服务器启动时默认加载Top Level的RabbitMQ配置，会报错，但不影响会使用。
 @SpringBootApplication(exclude = {RabbitAutoConfiguration.class})
-public class StreamMQMain8801 {
+public class StreamMQMain8802 {
     public static void main(String[] args) {
-        SpringApplication.run(StreamMQMain8801.class, args);
+        SpringApplication.run(StreamMQMain8802.class, args);
     }
 }
