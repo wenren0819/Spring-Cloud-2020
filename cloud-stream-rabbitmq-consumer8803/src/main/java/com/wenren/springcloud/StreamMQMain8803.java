20490dcb159268b54d7e8fc02a030a85eaeffe3e
wenren <r0I2O520>
2020-05-09 15:33:12 +0800

    cloud-stream-rabbitmq-consumer8803搭建完成。

--- cloud-stream-rabbitmq-consumer8803/src/main/java/com/wenren/springcloud/StreamMQMain8803.java
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/wenren/springcloud/StreamMQMain8803.java
@@ -3,16 +3,15 @@ package com.wenren.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author wangyang
  * @version V1.0
- * @create 2020-05-08-14:14
+ * @create 2020-05-09-15:08
  */
 @SpringBootApplication(exclude = {RabbitAutoConfiguration.class})
-public class StreamMQMain8802 {
+public class StreamMQMain8803 {
     public static void main(String[] args) {
-        SpringApplication.run(StreamMQMain8802.class, args);
+        SpringApplication.run(StreamMQMain8803.class, args);
     }
 }
