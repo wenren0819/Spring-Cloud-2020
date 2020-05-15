5bac2c10760ef16bc0ef944f0932237c96448fd7
wenren <r0I2O520>
2020-05-15 22:30:42 +0800

    cloudalibaba-consumer-nacos-client3377搭建完成。

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/wenren/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.wenren.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wangyang
+ * @version V1.0
+ * @create 2020-05-14-14:44
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/wenren/springcloud/alibaba/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.wenren.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wangyang
+ * @version V1.0
+ * @create 2020-05-14-14:44
+ */
+@RestController
+@RefreshScope //支持Nacos的动态刷新功能。
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
